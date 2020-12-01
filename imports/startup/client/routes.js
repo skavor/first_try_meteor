9ca96726ca294e8a3319465244606b2d0038e950
skavor <iskander.thabet@ensi-uma.tn>
2020-12-01 10:59:25 +0100

    add header & home components

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,13 +1,20 @@
 import { FlowRouter } from "meteor/kadira:flow-router";
 
 import '/imports/ui/layouts/outside/outside.js';
+import '../../ui/header/header';
+import '../../ui/home/home';
 import '../../ui/layouts/inside/inside';
 import '/imports/ui/login/login.js';
 import '../../ui/resolutions/resolutions';
 
+FlowRouter.route('/', {
+    action: function(params, queryParams) {
+        BlazeLayout.render('OutsideLayout', {top: 'Header', main: 'Home'});
+    }
+});
 FlowRouter.route('/login', {
     action: function(params, queryParams) {
-        BlazeLayout.render('OutsideLayout', {main: 'Login', title: 'Login Page'});
+        BlazeLayout.render('OutsideLayout', {top: 'Header', main: 'Login'});
     }
 });
 
