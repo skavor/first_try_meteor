ff397921c238760f2844c70e0628b446e0978244
skavor <iskander.thabet@ensi-uma.tn>
2020-12-04 11:12:29 +0100

    add publish & subscribe for slang

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -4,4 +4,6 @@ import '/imports/api/resolutions/methods.js';
 import '../../api/resolutions/publish';
 /**********SLANGS ********/
 import '../../api/slags/methods';
+import '../../api/slags/publish';
+/**********USERS**************/
 import '../../api/users/methods';
